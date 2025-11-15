435eb8adc1c998f6cb407cd6690b8814527cb49c
jwlee <lemonade4813@gmail.com>
2025-11-15 21:23:02 +0900

    fix : 캘린더 수정 (클릭시 닫힘 현상 해결).

--- src/app/components/SearchOptionSelector.tsx
+++ src/app/components/SearchOptionSelector.tsx
@@ -11,18 +11,25 @@ export default function SearchOptionSelector() {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className={styles.bannerCriteriaItemContainer}
-             onClick={() => setOpen((prev) => !prev)}
-             style={{ position : 'relative'}}
-        >
-            <Image src={calendar} alt="달력 이미지" />
-            <div className={styles.criteriaField}>
-                <p>언제 떠나세요?</p>
-                <p>4월 30일 - 5월 2일</p>
+        <div    
+            className={styles.bannerCriteriaItemContainer} 
+            style={{ position: 'relative' }}>
+            <div onClick={() => setOpen(prev => !prev)}>
+                <Image src={calendar} alt="달력 이미지" />
+                <div className={styles.criteriaField}>
+                    <p>언제 떠나세요?</p>
+                    <p>4월 30일 - 5월 2일</p>
+                </div>
             </div>
-            <div style={{ position : 'absolute'}}>
-            {open &&  <DateSelect onClose={() => setOpen(false)} />}
-            </div>
+            {open && (
+                <div style={{ position: 'absolute' }}>
+                    <DateSelect onClose={() => setOpen(false)} />
+                </div>
+            )}
         </div>
+    
     )
 }
+
+
+
